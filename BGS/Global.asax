fe3a692dcd4f4b294c0504b863b9bea922799209
Phil Houston <Philip.Houston@adp.com>
2017-03-18 19:17:34 +0000

    Add project files.

---
+++ BGS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BGS.MvcApplication" Language="C#" %>
---
+++ BGS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BGS.Startup))]
+namespace BGS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
